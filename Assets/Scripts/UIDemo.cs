caab88e0174028157e3eca2a713052d6baad004b
Sabina Beissenbekova <beissenb@shernet.sheridancollege.ca>
2026-02-04 11:41:47 -0500

    completed the coding gym, saved the scene

--- Assets/Scripts/UIDemo.cs
+++ Assets/Scripts/UIDemo.cs
@@ -1,14 +1,21 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class UIDemo : MonoBehaviour
 {
     SpriteRenderer sr;
+    public Image image; //the image on the UI canvas
+    public int howManyClicks = 0;
+    public TextMeshProUGUI score;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
+        sr = GetComponent<SpriteRenderer>();     
+        score.text = howManyClicks.ToString();
     }
 
     // Update is called once per frame
@@ -21,13 +28,30 @@ public class UIDemo : MonoBehaviour
         
     }
 
+    public void SetScaleBig(float scale)
+    {
+        transform.localScale = Vector3.one * scale;
+    }
+
     public void ChangeColor()
     {
         sr.color = Random.ColorHSV();
+        image.color = sr.color;
     }
 
     public void SetSize(float size)
     {
         transform.localScale = Vector3.one * size;
     }
+
+    public void AddToTheNumber()
+    {
+        howManyClicks += 1;
+        score.text = howManyClicks.ToString();
+    }
+
+    //public void SetRotation(float size)
+    //{
+    //    transform.eulerAngles = Vector3.one * size; //not completed, check out how rotation works
+    //}
 }
