ad326f63e70fed8f7bc0852f831daab8d6c13332
Sabina Beissenbekova <beissenb@shernet.sheridancollege.ca>
2026-02-11 09:56:04 -0500

    created new scene, made the tank move with controllers

---
+++ Assets/Scripts/ControllersMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllersMovement : MonoBehaviour
+{
+    public bool anyKeyIsPressed = false;
+    public float speed = 3;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        anyKeyIsPressed = Keyboard.current.anyKey.isPressed;
+
+        //test for left arrow key: move to the left
+        if (Keyboard.current.leftArrowKey.isPressed == true)
+        {
+            Vector2 newPos = transform.position;
+            newPos.x -= speed * Time.deltaTime;
+            transform.position = newPos;
+        }
+
+        //test for right arrow key: move to the right
+        if (Keyboard.current.rightArrowKey.isPressed == true)
+        {
+            Vector2 newPos = transform.position;
+            newPos.x += speed * Time.deltaTime;
+            transform.position = newPos;
+        }
+
+        
+    }
+}
