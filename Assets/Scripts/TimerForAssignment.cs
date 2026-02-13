2fa9f25eeef81e92d51f358d14102b7582547170
Sabina Beissenbekova <beissenb@shernet.sheridancollege.ca>
2026-02-13 11:19:03 -0500

    made a timer

---
+++ Assets/Scripts/TimerForAssignment.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class TimerForAssignment : MonoBehaviour
+{
+    public float time = 100;
+    public float timeEnd = 0;
+    public TextMeshProUGUI timerVisuals;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime; 
+
+        if(time < timeEnd) //if the time counts to 0 it starts from the beginning
+        {
+            time = 100;
+        }
+
+        timerVisuals.text = time.ToString(); //so the text will show the amount of time left
+    }
+}
